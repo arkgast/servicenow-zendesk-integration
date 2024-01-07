1d10f16d3c7d672f097bbb4c7267fd11786e9e9f
Arnold Gandarillas Castillo <arkgast@gmail.com>
2024-01-06 22:27:13 -0400

    feat: send request to create bundle

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -3,22 +3,44 @@ package routes
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"minka/support/pkg/common"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+var URL = "https://minkasupport.zendesk.com"
+
 func CreateBundle(c *fiber.Ctx) error {
 	log.Println("Creating zendesk bundle...")
 
-	content, err := common.ReadAndParseJSON("bundle.json")
+	content, err := common.ReadFile("bundle.json")
 	if err != nil {
 		log.Println("Error reading bundle.json file")
 		return c.SendStatus(http.StatusInternalServerError)
 	}
 
-	return c.JSON(content)
+	url := URL + "/api/services/zis/registry/service-now-dev/bundles"
+	username := os.Getenv("ZENDESK_USERNAME")
+	password := os.Getenv("ZENDESK_PASSWORD")
+
+	agent := fiber.Post(url)
+	agent.Set("Content-Type", "application/json")
+	agent.Body(content)
+	agent.BasicAuth(username, password)
+
+	statusCode, _, errs := agent.Bytes()
+
+	if len(errs) > 0 {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"errs": errs,
+		})
+	}
+
+	log.Printf("Status code: %d", statusCode)
+
+	return c.SendStatus(http.StatusOK)
 }
 
 func UpdateTask(c *fiber.Ctx) error {
